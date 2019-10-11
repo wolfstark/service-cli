0679ea5c129d44e0124d114ddb799b1cc3d6151a
wolfstark <1038999405@qq.com>
2019-10-11 10:12:57 +0800

    feat: 🎸 update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,15 @@
+/*
+ * @Author: Wang Xiang
+ * @Date: 2019-10-11 10:09:23
+ * @LastEditTime: 2019-10-11 10:09:23
+ * @LastEditors: Wang Xiang
+ */
 module.exports = {
     env: {
         commonjs: true,
         es6: true,
-        node: true
+        node: true,
+        jest: true
     },
     extends: ["airbnb-base", "plugin:prettier/recommended"],
     globals: {
