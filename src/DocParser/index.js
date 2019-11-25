7c35eb3f37a20d87b9731cab37e0de3dce42b7ad
wolfstark <1038999405@qq.com>
2019-11-25 18:14:30 +0800

    update

--- src/DocParser/index.js
+++ src/DocParser/index.js
@@ -1,14 +1,34 @@
 /*
  * @Author: Wang Xiang
  * @Date: 2019-10-22 09:30:09
- * @LastEditTime: 2019-11-12 09:38:14
+ * @LastEditTime: 2019-11-25 16:09:08
  * @LastEditors: Wang Xiang
  */
-const OriginType = {};
-OriginType.SwaggerV3 = "SwaggerV3";
-OriginType.SwaggerV2 = "SwaggerV2";
-OriginType.SwaggerV1 = "SwaggerV1";
+import DataSourceConfig from "../Config/DataSourceConfig";
+
+const OriginType = {
+    SwaggerV3: "SwaggerV3",
+    SwaggerV2: "SwaggerV2",
+    SwaggerV1: "SwaggerV1"
+};
+/**
+ * 获取远端数据并解析
+ *
+ * @param {DataSourceConfig} config
+ * @param {*} report
+ * @returns
+ */
+async function readRemoteDataSource(config) {
+    switch (config.originType) {
+        case OriginType.SwaggerV2: {
+            return new SwaggerV2Reader(config, report).fetchRemoteData();
+        }
+        default:
+            return new SwaggerV2Reader(config, report).fetchRemoteData();
+    }
+}
 
 module.exports = {
-    OriginType
+    OriginType,
+    readRemoteDataSource
 };
